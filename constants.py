aa2c6790eb9870289e2b56d9c70dd7fb76a4df24
Zhisheng Ye <yezhisheng@pjlab.org.cn>
2023-03-31 00:06:56 +0800

    init

---
+++ constants.py
@@ -0,0 +1,14 @@
+'''Copyright The Microsoft DeepSpeed Team'''
+from .accelerator import accelerator
+
+def get_accelerator():
+    return accelerator
+
+DEFAULT_WARMUPS = 5
+DEFAULT_TRIALS = 50
+DEFAULT_TYPE = 'float'
+DEFAULT_BACKEND = get_accelerator().communication_backend_name()
+DEFAULT_UNIT = 'Gbps'
+DEFAULT_DIST = 'deepspeed'
+DEFAULT_MAXSIZE = 24
+TORCH_DISTRIBUTED_DEFAULT_PORT = 29500
